4df318a4c40a069890ef14df71dbe9740581e9e1
Wingfung Hung <1039377283@qq.com>
2023-04-26 22:04:17 +0800

    fix(QueryActivity.java):完善了搜索功能对EditText的监听功能

--- app/src/main/java/com/hongyongfeng/wanandroid/module/query/view/QueryActivity.java
+++ app/src/main/java/com/hongyongfeng/wanandroid/module/query/view/QueryActivity.java
@@ -1,7 +1,11 @@
 package com.hongyongfeng.wanandroid.module.query.view;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -11,7 +15,11 @@ import com.hongyongfeng.wanandroid.base.BaseActivity;
 import com.hongyongfeng.wanandroid.module.query.interfaces.Query;
 import com.hongyongfeng.wanandroid.module.query.presenter.QueryPresenter;
 
-public class QueryActivity extends BaseActivity<QueryPresenter, Query.VP> {
+public class QueryActivity extends BaseActivity<QueryPresenter, Query.VP>{
+
+    TextView tvBack;
+    TextView tvClear;
+    EditText edtKeyWords;
 
     @Override
     public Query.VP getContract() {
@@ -20,7 +28,31 @@ public class QueryActivity extends BaseActivity<QueryPresenter, Query.VP> {
 
     @Override
     public void initListener() {
+        tvClear.setOnClickListener(this);
+        tvBack.setOnClickListener(this);
+        edtKeyWords.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (edtKeyWords.getText().toString().length()>0){
+                    tvClear.setVisibility(View.VISIBLE);
+                }else {
+                    tvClear.setVisibility(View.INVISIBLE);
+                }
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (edtKeyWords.getText().toString().length()>0){
+                    tvClear.setVisibility(View.VISIBLE);
+                }else {
+                    tvClear.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 
     @Override
@@ -51,16 +83,30 @@ public class QueryActivity extends BaseActivity<QueryPresenter, Query.VP> {
 
     @Override
     public void initView() {
-
+        tvBack=findViewById(R.id.tv_back);
+        tvClear=findViewById(R.id.tv_clear);
+        edtKeyWords=findViewById(R.id.edt_keyword);
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.tv_clear:
+                edtKeyWords.setText("");
+                break;
+            case R.id.tv_back:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
+
+
 }
