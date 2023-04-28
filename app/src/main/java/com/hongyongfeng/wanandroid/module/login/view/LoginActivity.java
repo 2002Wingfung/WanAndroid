c008494c5616fe6fc6e23445f1ba838b4aafe0cc
Wingfung Hung <1039377283@qq.com>
2023-04-28 12:45:02 +0800

    fix(LoginActivity,activity_login.xml):完善了登录界面

--- app/src/main/java/com/hongyongfeng/wanandroid/module/login/view/LoginActivity.java
+++ app/src/main/java/com/hongyongfeng/wanandroid/module/login/view/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.hongyongfeng.wanandroid.module.login.view;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import com.hongyongfeng.wanandroid.R;
 import com.hongyongfeng.wanandroid.base.BaseActivity;
 import com.hongyongfeng.wanandroid.module.login.interfaces.ILogin;
@@ -31,6 +39,63 @@ public class LoginActivity extends BaseActivity<LoginPresenter,ILogin.VP> {
     @Override
     public void initListener() {
         btnLogin.setOnClickListener(this);
+        edtPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (edtPwd.getText().toString().length()>0){
+                    tvPwd.setVisibility(View.INVISIBLE);
+                    password=1;
+                    if (name==1){
+                        btnLogin.setEnabled(true);
+                        btnLogin.setBackground(getResources().getDrawable(R.drawable.button_shape));
+                    }
+
+                }else {
+                    tvPwd.setVisibility(View.VISIBLE);
+                    password=0;
+                    btnLogin.setEnabled(false);
+                    btnLogin.setBackground(getResources().getDrawable(R.drawable.bg_btn_login));
+                }
+            }
+        });
+        edtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (edtName.getText().toString().length()>0){
+                    tvAccount.setVisibility(View.INVISIBLE);
+                    name=1;
+                    if (password==1){
+                        btnLogin.setEnabled(true);
+                        btnLogin.setBackground(getResources().getDrawable(R.drawable.button_shape));
+                    }
+
+                }else {
+                    tvAccount.setVisibility(View.VISIBLE);
+                    name=0;
+                    btnLogin.setEnabled(false);
+                    btnLogin.setBackground(getResources().getDrawable(R.drawable.bg_btn_login));
+                }
+            }
+        });
+        tvVisibility.setOnClickListener(this);
     }
 
 
@@ -64,32 +129,64 @@ public class LoginActivity extends BaseActivity<LoginPresenter,ILogin.VP> {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        imgAccount.setColorFilter(0xff838383);
+//        imgAccount.setColorFilter(0xff838383);
     }
 
-    private EditText etName;
-    private EditText etPwd;
+    private int name=0;
+    private int password=0;
+    private TextView tvPwd;
+    private TextView tvAccount;
+    private EditText edtName;
+    private EditText edtPwd;
     private Button btnLogin;
-
+    private ConstraintLayout layout;
     private ImageView imgAccount;
+    private TextView tvVisibility;
     @Override
     public void initView() {
-        etName=findViewById(R.id.login_user_name);
-        etPwd=findViewById(R.id.login_password);
+        tvVisibility=findViewById(R.id.password_visibility);
+        edtName =findViewById(R.id.edt_login_user_name);
+        edtPwd =findViewById(R.id.edt_login_password);
         btnLogin=findViewById(R.id.login);
         imgAccount=findViewById(R.id.account);
+        layout=findViewById(R.id.include);
+        layout.findViewById(R.id.tv_menu).setVisibility(View.INVISIBLE);
+        layout.findViewById(R.id.tv_query).setVisibility(View.INVISIBLE);
+        TextView tvTitle=layout.findViewById(R.id.tv_title);
+        //tvTitle.setText("登录");
+        tvAccount=findViewById(R.id.account_warning);
+        tvPwd=findViewById(R.id.password_warning);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        String name=etName.getText().toString();
-        String pwd=etPwd.getText().toString();
+        String name= edtName.getText().toString();
+        String pwd= edtPwd.getText().toString();
+        final int[] count = {0};
+        int num2=2;
+        int num0=0;
+        switch (v.getId()){
+            case R.id.password_visibility:
+                if (count[0] %num2==num0){
+                    edtPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    tvVisibility.setBackground(getResources().getDrawable(R.drawable.ic_visible));
+                }
+                else {
+                    edtPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    tvVisibility.setBackground(getResources().getDrawable(R.drawable.ic_invisible));
+                }
+                count[0]++;
+                break;
+            default:
+                break;
+        }
         //第一种方式：面向具体方法
 
         //requestLogin(name,pwd);
 
         //第二种方式：面向接口
-        getContract().requestLoginVP(name,pwd);
+        //getContract().requestLoginVP(name,pwd);
     }
 
 }
