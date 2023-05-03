3599af7e133724245c4d0a72375c8209e82b9fe4
Wingfung Hung <1039377283@qq.com>
2023-05-03 13:33:10 +0800

    feat(login):新增了注册界面的viewpager

--- app/src/main/java/com/hongyongfeng/wanandroid/module/signinorup/SignInUpActivity.java
+++ app/src/main/java/com/hongyongfeng/wanandroid/module/signinorup/SignInUpActivity.java
@@ -20,6 +20,8 @@ import com.hongyongfeng.wanandroid.R;
 import com.hongyongfeng.wanandroid.base.BaseActivity;
 import com.hongyongfeng.wanandroid.module.signinorup.login.interfaces.ILogin;
 import com.hongyongfeng.wanandroid.module.signinorup.login.presenter.LoginPresenter;
+import com.hongyongfeng.wanandroid.module.signinorup.login.view.fragment.LoginFragment;
+import com.hongyongfeng.wanandroid.module.signinorup.register.fragment.RegisterFragment;
 import com.hongyongfeng.wanandroid.test.FragmentVPAdapter;
 import com.hongyongfeng.wanandroid.test.VPFragment;
 
@@ -97,12 +99,10 @@ public class SignInUpActivity extends BaseActivity<LoginPresenter,ILogin.VP> {
     @Override
     public void initData() {
         fragmentList=new ArrayList<>();
-        VPFragment fragmentHome=VPFragment.newInstance("首页文章","");
-        VPFragment fragmentHome1=VPFragment.newInstance("nihao","");
 
-        fragmentList.add(fragmentHome);
-        fragmentList.add(fragmentHome1);
-        //fragmentList.add(new LoginFragment());
+        fragmentList.add(new LoginFragment());
+
+        fragmentList.add(new RegisterFragment());
     }
 
     @Override
@@ -131,80 +131,26 @@ public class SignInUpActivity extends BaseActivity<LoginPresenter,ILogin.VP> {
         super.onCreate(savedInstanceState);
         adapter=new FragmentVPAdapter(getSupportFragmentManager(),fragmentList);
         viewPager.setAdapter(adapter);
-//        imgAccount.setColorFilter(0xff838383);
-
     }
     private List<Fragment> fragmentList;
     private FragmentVPAdapter adapter;
     private ViewPager viewPager;
-
-    private int name=0;
-    private int password=0;
-    private TextView tvPwd;
-    private TextView tvAccount;
-    private EditText edtName;
-    private EditText edtPwd;
-    private Button btnLogin;
-    private ConstraintLayout layout;
-    private ImageView imgAccount;
-    private TextView tvVisibility;
     TextView tvLogin;
     TextView tvRegister;
     TextView tvBack;
-
-    final int[] count = {0};
-
     @Override
     public void initView() {
-//        tvVisibility=findViewById(R.id.password_visibility);
-//        edtName =findViewById(R.id.edt_login_user_name);
-//        edtPwd =findViewById(R.id.edt_login_password);
-//        btnLogin=findViewById(R.id.login);
-//        imgAccount=findViewById(R.id.account);
-//        layout=findViewById(R.id.include);
         tvBack=findViewById(R.id.tv_back);
         tvLogin=findViewById(R.id.tv_login);
         tvRegister=findViewById(R.id.tv_register);
-//        //tvTitle.setText("登录");
-//        tvAccount=findViewById(R.id.account_warning);
-//        tvPwd=findViewById(R.id.password_warning);
-//        edtPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
         viewPager=findViewById(R.id.vp_login_and_register);
-
-
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        int num2 = 2;
-        int num0 = 0;
-        switch (v.getId()){
-            case R.id.password_visibility:
-                if (count[0] % num2 == num0){
-                    tvVisibility.setBackground(getResources().getDrawable(R.drawable.ic_visible));
-                    edtPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-
-                }
-                else {
-                    tvVisibility.setBackground(getResources().getDrawable(R.drawable.ic_invisible));
-                    edtPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
-
-                }
-                count[0]++;
-                break;
-            case R.id.tv_back:
-                finish();
-                break;
-            case R.id.login:
-                String name= edtName.getText().toString().replaceAll(" ","");
-                String pwd= edtPwd.getText().toString().replaceAll(" ","");
-                System.out.println("name"+name);
-                System.out.println("pwd"+pwd);
-                //面向接口
-                getContract().requestLoginVP(name,pwd);
-            default:
-                break;
+        if (v.getId() == R.id.tv_back) {
+            finish();
         }
     }
 }
---
+++ app/src/main/java/com/hongyongfeng/wanandroid/module/signinorup/register/fragment/RegisterFragment.java
@@ -0,0 +1,111 @@
+package com.hongyongfeng.wanandroid.module.signinorup.register.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+import com.hongyongfeng.wanandroid.R;
+import com.hongyongfeng.wanandroid.base.BaseFragment;
+import com.hongyongfeng.wanandroid.module.signinorup.login.interfaces.ILogin;
+import com.hongyongfeng.wanandroid.module.signinorup.login.presenter.LoginFragmentPresenter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RegisterFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RegisterFragment extends BaseFragment<LoginFragmentPresenter, ILogin.VP> {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    public static RegisterFragment newInstance(String param1, String param2) {
+        RegisterFragment fragment = new RegisterFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public ILogin.VP getContract() {
+        return new ILogin.VP() {
+            @Override
+            public void requestLoginVP(String name, String pwd) {
+
+            }
+
+            @Override
+            public void responseLoginResult(boolean loginStatusResult) {
+
+            }
+        };
+    }
+
+
+    @Override
+    protected void destroy() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected LoginFragmentPresenter getPresenterInstance() {
+        return new LoginFragmentPresenter();
+    }
+
+    @Override
+    protected <ERROR> void responseError(ERROR error, Throwable throwable) {
+
+    }
+
+    @Override
+    protected int getFragmentView() {
+        return R.layout.fragment_register;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
