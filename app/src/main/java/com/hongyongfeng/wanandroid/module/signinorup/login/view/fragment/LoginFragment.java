b9c25c31d798594484dbc2fbec5c78f93498712e
Wingfung Hung <1039377283@qq.com>
2023-05-03 13:42:26 +0800

    fix(login):完善了登录Fragment的事件监听

--- app/src/main/java/com/hongyongfeng/wanandroid/module/signinorup/login/view/fragment/LoginFragment.java
+++ app/src/main/java/com/hongyongfeng/wanandroid/module/signinorup/login/view/fragment/LoginFragment.java
@@ -1,12 +1,24 @@
 package com.hongyongfeng.wanandroid.module.signinorup.login.view.fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hongyongfeng.wanandroid.R;
 import com.hongyongfeng.wanandroid.base.BaseFragment;
@@ -15,6 +27,7 @@ import com.hongyongfeng.wanandroid.module.knowledge.presenter.KnowledgeFragmentP
 import com.hongyongfeng.wanandroid.module.signinorup.login.interfaces.ILogin;
 import com.hongyongfeng.wanandroid.module.signinorup.login.presenter.LoginFragmentPresenter;
 import com.hongyongfeng.wanandroid.module.signinorup.login.presenter.LoginPresenter;
+import com.hongyongfeng.wanandroid.module.signinorup.login.view.LoginActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +41,17 @@ public class LoginFragment extends BaseFragment<LoginFragmentPresenter, ILogin.V
 
     private String mParam1;
     private String mParam2;
+    private Button btnLogin;
+    private TextView tvVisibility;
+    private EditText edtPwd;
+    private EditText edtName;
+    private TextView tvAccount;
+    private TextView tvPwd;
+    private int name=0;
+    private int password=0;
+    private FragmentActivity fragmentActivity;
+    final int[] count = {0};
+
 
     public LoginFragment() {
         // Required empty public constructor
@@ -60,15 +84,23 @@ public class LoginFragment extends BaseFragment<LoginFragmentPresenter, ILogin.V
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        fragmentActivity=requireActivity();
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
     public ILogin.VP getContract() {
         return new ILogin.VP() {
             @Override
             public void requestLoginVP(String name, String pwd) {
-
+                mPresenter.getContract().requestLoginVP(name,pwd);
             }
 
             @Override
             public void responseLoginResult(boolean loginStatusResult) {
+                Toast.makeText(fragmentActivity, loginStatusResult?"登录成功":"登录失败", Toast.LENGTH_SHORT).show();
 
             }
         };
@@ -82,12 +114,98 @@ public class LoginFragment extends BaseFragment<LoginFragmentPresenter, ILogin.V
 
     @Override
     protected void initView(View view) {
-
+        tvVisibility=fragmentActivity.findViewById(R.id.password_visibility);
+        edtName =fragmentActivity.findViewById(R.id.edt_login_user_name);
+        edtPwd =fragmentActivity.findViewById(R.id.edt_login_password);
+        btnLogin=fragmentActivity.findViewById(R.id.login);
+//        layout=findViewById(R.id.include);
+//        tvBack=layout.findViewById(R.id.tv_back);
+//        tvLogin=layout.findViewById(R.id.tv_login);
+//        tvRegister=layout.findViewById(R.id.tv_register);
+        //tvTitle.setText("登录");
+        tvAccount=fragmentActivity.findViewById(R.id.account_warning);
+        tvPwd=fragmentActivity.findViewById(R.id.password_warning);
+        edtPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
     }
 
     @Override
     protected void initListener() {
+        btnLogin.setOnClickListener(this);
+        edtPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().contains(" ")){
+                    String[] str = s.toString().split(" ");
+                    StringBuilder content = new StringBuilder();
+                    for (String value : str) {
+                        content.append(value);
+                    }
+                    edtPwd.setText(content.toString());
+                    edtPwd.setSelection(start);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (edtPwd.getText().toString().length()>0){
+                    tvPwd.setVisibility(View.INVISIBLE);
+                    password=1;
+                    if (name==1){
+                        btnLogin.setEnabled(true);
+                        btnLogin.setBackground(getResources().getDrawable(R.drawable.button_shape));
+                    }
+
+                }else {
+                    tvPwd.setVisibility(View.VISIBLE);
+                    password=0;
+                    btnLogin.setEnabled(false);
+                    btnLogin.setBackground(getResources().getDrawable(R.drawable.bg_btn_login));
+                }
+            }
+        });
+        edtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().contains(" ")){
+                    String[] str = s.toString().split(" ");
+                    StringBuilder content = new StringBuilder();
+                    for (String value : str) {
+                        content.append(value);
+                    }
+                    edtName.setText(content.toString());
+                    edtName.setSelection(start);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (edtName.getText().toString().length()>0){
+                    tvAccount.setVisibility(View.INVISIBLE);
+                    name=1;
+                    if (password==1){
+                        btnLogin.setEnabled(true);
+                        btnLogin.setBackground(getResources().getDrawable(R.drawable.button_shape));
+                    }
+
+                }else {
+                    tvAccount.setVisibility(View.VISIBLE);
+                    name=0;
+                    btnLogin.setEnabled(false);
+                    btnLogin.setBackground(getResources().getDrawable(R.drawable.bg_btn_login));
+                }
+            }
+        });
+        tvVisibility.setOnClickListener(this);
     }
 
     @Override
@@ -110,8 +228,34 @@ public class LoginFragment extends BaseFragment<LoginFragmentPresenter, ILogin.V
         return R.layout.fragment_login;
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
+        int num2 = 2;
+        int num0 = 0;
+        switch (v.getId()){
+            case R.id.password_visibility:
+                if (count[0] % num2 == num0){
+                    tvVisibility.setBackground(getResources().getDrawable(R.drawable.ic_visible));
+                    edtPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
 
+                }
+                else {
+                    tvVisibility.setBackground(getResources().getDrawable(R.drawable.ic_invisible));
+                    edtPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+                }
+                count[0]++;
+                break;
+            case R.id.login:
+                String name= edtName.getText().toString().replaceAll(" ","");
+                String pwd= edtPwd.getText().toString().replaceAll(" ","");
+                System.out.println("name"+name);
+                System.out.println("pwd"+pwd);
+                //面向接口
+                getContract().requestLoginVP(name,pwd);
+            default:
+                break;
+        }
     }
 }
